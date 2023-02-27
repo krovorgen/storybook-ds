ea039ceefaf75074896b01e46ec622630e4485a3
maksim <krovorgen@gmail.com>
2023-02-28 01:46:07 +0300

    fix btn

--- stories/Button.js
+++ stories/Button.js
@@ -1,14 +1,12 @@
 import './button.scss';
 
-export const createButton = ({ mode, size, backgroundColor, label, onClick }) => {
+export const createButton = ({ mode, size, label, onClick }) => {
   const btn = document.createElement('button');
   btn.type = 'button';
   btn.innerText = label;
   btn.addEventListener('click', onClick);
 
   btn.className = ['button', `button--${size}`, `button--${mode}`].join(' ');
-
-  btn.style.backgroundColor = backgroundColor;
 
   return btn;
 };
